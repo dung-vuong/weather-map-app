676cf4cd8cb9f94fe9ad33cf719da098d8cd3ddc
Dung Vuong <dung.vuong@sjsu.edu>
2022-03-19 00:14:32 -0700

    update

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import GoogleMapReact from 'google-map-react'
 import {Paper, Typography, useMediaQuery} from '@material-ui/core'
-import LocationOnOutlinedIcon from "@material-ui/icons/LocationOutlined";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from '@material-ui/lab'
 
 import useStyles from './styles'
@@ -9,10 +9,18 @@ import useStyles from './styles'
 const Map = () => {
     const classes = useStyles()
     const isMobile = useMediaQuery('(min-width:600px)')
+    const coordinates = {lat: 0, lng: 0}
     return(
-        <div className="classes.mapContainer">
+        <div className={classes.mapContainer}>
             <GoogleMapReact 
-                bootstrapURLKeys={{key: ''}}
+                bootstrapURLKeys={{key: 'AIzaSyDK0rpS55Cs3BaRimq0M-hdYbzz5UzfBWE'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                // options={''}
+                // onChange={}
+                // onChildClick={''}
             >
 
             </GoogleMapReact>
--- src/components/Map/styles.js
+++ src/components/Map/styles.js
@@ -1,28 +1,16 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1), 
-    minWidth: 120, 
-    marginBottom: '30px',
+export default makeStyles(() => ({
+  paper: {
+    padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100px',
   },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
+  mapContainer: {
+    height: '85vh', width: '100%',
   },
-  loading: {
-    height: '600px', 
-    display: 'flex', 
-    justifyContent: 'center', 
-    alignItems: 'center',
+  markerContainer: {
+    position: 'absolute', transform: 'translate(-50%, -50%)', zIndex: 1, '&:hover': { zIndex: 2 },
   },
-  container: {
-    padding: '25px',
-  },
-  marginBottom: {
-    marginBottom: '30px',
-  },
-  list: {
-    height: '75vh', 
-    overflow: 'auto',
+  pointer: {
+    cursor: 'pointer',
   },
 }));
