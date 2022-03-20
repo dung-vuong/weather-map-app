a7375d90643ec4f63c33863d127303036c82b330
Dung Vuong <dungcoder1213@gmail.com>
2022-03-20 00:32:14 -0700

    get name

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ function App() {
 
   const [places, setPlaces] = useState([])
   const [coordinates, setCoordinates] = useState({})
-  const [bounds, setBounds] = useState(null)
+  const [bounds, setBounds] = useState({})
 
+  //GET CURRENT LOCATION
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
       setCoordinates({lat: latitude, lng: longitude})
@@ -22,8 +23,7 @@ function App() {
   }, [])
 
   useEffect(() => {
-    console.log(coordinates, bounds)
-    getPlacesData()
+    getPlacesData(bounds.sw, bounds.ne)
       .then((data) => {
         console.log(data)
         setPlaces(data)
@@ -36,7 +36,9 @@ function App() {
       <Header/>
       <Grid container spacing={3} style={{width: '100%'}}>
         <Grid item xs={12} md={4}>
-          <List/>
+          <List
+            places={places}
+          />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
--- src/api/index.js
+++ src/api/index.js
@@ -2,22 +2,21 @@ import axios from "axios";
 
 const URL = "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary"
 
-const options = {
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-  },
-  headers: {
-    'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-    'x-rapidapi-key': '1903c37bf5mshed9fca3d44d7538p19574bjsn7a0990036aab'
-  }
-};
-
-const getPlacesData = async() => {
+const getPlacesData = async(sw,ne) => {
     try {
-        const {data: {data}} = await axios.get(URL, options)
+        const {data: {data}} = await axios.get(URL, {
+            //SET THE southwest and northeast LAT and LNG
+            params: {
+              bl_latitude: sw.lat,
+              tr_latitude: ne.lat,
+              bl_longitude: sw.lng,
+              tr_longitude: ne.lng,
+            },
+            headers: {
+              'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+              'x-rapidapi-key': '1903c37bf5mshed9fca3d44d7538p19574bjsn7a0990036aab'
+            }
+          })
         return data
     } catch (error) {
         console.log(error)
