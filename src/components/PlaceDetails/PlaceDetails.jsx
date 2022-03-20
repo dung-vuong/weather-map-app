a54297e061b179445f7d8bfc6a1bd24e137d8e04
Dung Vuong <dungcoder1213@gmail.com>
2022-03-20 02:18:30 -0700

    Card content

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip} from '@material-ui/core'
+import {Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip} from '@material-ui/core'
 import LocationOnIcon from '@material-ui/icons/LocationOn'
 import PhoneIcon from '@material-ui/icons/Phone'
 import Rating from '@material-ui/lab/Rating'
@@ -14,6 +14,46 @@ const PlaceDetails = ({place}) => {
                 image={place.photo ? place.photo.images.large.url : 'https://www.bbrollingdoor.com/upload/services/mainimages/Restuarant1.jpg'}
                 title={place.name}
             />
+            <CardContent>
+                <Typography gutterBottom varian="h5">
+                    {place.name}
+                </Typography>
+                <Box display="flex"justifyContent="space-between">
+                    <Typography variant="subtitle1">Price</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+                </Box>
+                <Box display="flex"justifyContent="space-between">
+                    <Typography variant="subtitle1">Ranking</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
+                </Box>
+                {place?.awards?.map((award) => (
+                    <Box my={1} display="flex" justifyContent="space-between" alignItems="center">
+                        <img src={award.images.small} alt={award.display_name}/>
+                        <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
+                    </Box>
+                ))}
+                {place?.cuisine?.map(({name}) => (
+                    <Chip key={name} size="small" label={name} className={classes.chip}/>
+                ))}
+                {place?.address && (
+                    <Typography gutterBottom variant="subtitle2" color="textSecondary" className={classes.subtitle}>
+                        <LocationOnIcon/>{place.address}
+                    </Typography>
+                )}
+                {place?.phone && (
+                    <Typography gutterBottom variant="subtitle2" color="textSecondary" className={classes.spacing}>
+                        <PhoneIcon/>{place.phone}
+                    </Typography>
+                )}
+                <CardActions>
+                    <Button size="small" color="primary" onClick={() => window.open(place.web_url, 'blank')}>
+                        Trip Advisor
+                    </Button>
+                    <Button size="small" color="primary" onClick={() => window.open(place.website, 'blank')}>
+                        Website
+                    </Button>
+                </CardActions>
+            </CardContent>
         </Card>
     )
 }
