7674f555ba4e8f15a353d3ac0b114d27a9acfb40
Dung Vuong <dungcoder1213@gmail.com>
2022-03-20 00:58:36 -0700

    card media

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import {box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip} from '@material-ui/core'
+import LocationOnIcon from '@material-ui/icons/LocationOn'
+import PhoneIcon from '@material-ui/icons/Phone'
+import Rating from '@material-ui/lab/Rating'
+import useStyles from './styles'
 
 const PlaceDetails = ({place}) => {
+    const classes = useStyles()
     return(
-        <h1>{place.name}</h1>
+        <Card elevation={6}>
+            <CardMedia 
+                style={{height: 350}}
+                image={place.photo ? place.photo.images.large.url : 'https://www.bbrollingdoor.com/upload/services/mainimages/Restuarant1.jpg'}
+                title={place.name}
+            />
+        </Card>
     )
 }
 
